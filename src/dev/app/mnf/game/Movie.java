4afd443f0df1d393e6b5b353bf98cab962efe700
Vasishta <vasishta.balla@gmail.com>
2021-05-08 23:55:16 +0530

    Update Movie.java
    test commit

--- src/dev/app/mnf/game/Movie.java
+++ src/dev/app/mnf/game/Movie.java
@@ -24,7 +24,7 @@ public class Movie {
 
     @Override
     public String toString() {
-        return "Movie{" +
+        return "Movie {" +
                 "title='" + title + '\'' +
                 ", cast=" + cast +
                 '}';
